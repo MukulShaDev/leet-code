0191df558b91e39d17763b98b6fcca53106e73b2
Solomon_Swan <42635739+MukulShaDev@users.noreply.github.com>
2023-11-30 19:39:05 -0800

    Time: 1 ms (99.78%), Space: 44.6 MB (64.33%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> answer = new ArrayList<>();
+        List<String> answer = new ArrayList<>(n);
         for(int i = 1; i <= n; i++){
             if(i % 3 == 0 && i % 5 == 0){
                 answer.add("FizzBuzz"); 
