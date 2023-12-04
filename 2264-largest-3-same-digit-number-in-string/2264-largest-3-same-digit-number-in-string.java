afab8cae32fdfdc947c8417d2d1f07a45e38e1fa
Solomon_Swan <42635739+MukulShaDev@users.noreply.github.com>
2023-12-03 19:10:57 -0800

    Time: 1 ms (91.57%), Space: 41.5 MB (27.33%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -1,18 +1,27 @@
 class Solution {
-    public String largestGoodInteger(String num) {
-        int max = 0;
-        String finalstr = "";
-        for(char ch : num.toCharArray()){
-            String check = String.valueOf(ch);
-            String combined = check+check+check;
-            if(num.contains(combined)){
-                int value = ch - '0';
-                if(value >= max){
-                    max = value;
-                    finalstr = String.valueOf(ch);
-                }
+    private List<String> sameDigitNumbers = List.of("999", "888", "777", "666", "555", "444", "333", "222", "111", "000");
+
+    // Check whether the 'num' string contains the 'sameDigitNumber' string or not.
+    private boolean contains(String sameDigitNumber, String num) {
+        for (int index = 0; index <= num.length() - 3; ++index) {
+            if (num.charAt(index) == sameDigitNumber.charAt(0) &&
+                num.charAt(index + 1) == sameDigitNumber.charAt(1) &&
+                num.charAt(index + 2) == sameDigitNumber.charAt(2)) {
+                return true;
             }
         }
-        return finalstr + finalstr + finalstr;
+        return false;
+    }
+
+    public String largestGoodInteger(String num) {
+        // Iterate on all 'sameDigitNumbers' and check if the string 'num' contains it.
+        for (String sameDigitNumber : sameDigitNumbers) {
+            if (contains(sameDigitNumber, num)) {
+                // Return the current 'sameDigitNumbers'.
+                return sameDigitNumber;
+            }
+        }
+        // No 3 consecutive same digits are present in the string 'num'.
+        return "";
     }
 }
